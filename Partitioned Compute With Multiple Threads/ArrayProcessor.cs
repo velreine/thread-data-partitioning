67e70a760b84adcc30a594cc21b11469cc057b42
Velreine <nicky-g-hansen@hotmail.com>
2019-05-24 22:11:34 +0200

    Add project files.

---
+++ Partitioned Compute With Multiple Threads/ArrayProcessor.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Partitioned_Compute_With_Multiple_Threads
+{
+    internal class ArrayProcessor
+    {
+        private readonly double[] _data;
+        private readonly int _firstIndex;
+        private readonly int _lastIndex;
+
+        public ArrayProcessor(double[] data, int firstIndex, int lastIndex)
+        {
+            _data = data;
+            _firstIndex = firstIndex;
+            _lastIndex = lastIndex;
+        }
+
+        public void ComputeSum()
+        {
+            Sum = 0;
+
+            for (int n = _firstIndex; n <= _lastIndex; n++)
+            {
+                Sum += _data[n];
+                Thread.Sleep(1);
+            }
+        }
+
+        public double Sum { get; private set; }
+    }
+}
